e0ebdd29ff0cddadfea34301cc1c3c313aa000a7
ismael <ismaeldefreitas@gmail.com>
2017-02-01 19:54:26 -0200

    Alterações de cor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Components são como tags html com conteúdo. É um pedaço de uma DOM com conteúdo e classe de estilo.
 class App extends Component {
   render() {
     return (
@@ -9,8 +10,14 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Bem-vindo ao React do Ismael</h2>
+          <h3>Estou dentro do component App</h3>
+          &nbsp;
+          <hr/>
+
         </div>
+
         <p className="App-intro">
+
          Para iniciar, edite <code>src/App.js</code>, depois salve para recarregar.
         </p>
       </div>
@@ -18,5 +25,5 @@ class App extends Component {
   }
 }
 
-
+//O export torna o component acessível para ser importado pela index.js.
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import Nav from './Nav';
 import App from './App';
 import './index.css';
 
-
+//Este index.js é como o main.js. Nele deve ser importado tudo o que se quer que seja renderizado na view.
 ReactDOM.render(
   <Nav 
   title="Test Props" 
   linkUrl="http://www.google.com.br"
-  texto="olá mundão!!"
+
   />,
   document.getElementById('nav')
 );
@@ -17,4 +17,10 @@ ReactDOM.render(
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+);
+
+
+ReactDOM.render(
+  <h1>Hello, mundão véi!</h1>,
+  document.getElementById('teste')
 );
